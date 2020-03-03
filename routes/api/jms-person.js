297a80a1b5863be7fd91bc431e3caf2ab1debd36
John <johnmsaylor@gmail.com>
2020-03-03 12:32:33 -0500

    get and post route

--- routes/api/jms-person.js
+++ routes/api/jms-person.js
@@ -5,3 +5,29 @@
 // push this branch to github ‘git push -u <remote> <branch>’
 // this branch needs to be merged to master. I can’t stop you from using git merge however this excersise is to practice pull requests. To complete this correctly merge the branch to master using the pull request system on your own repo
 // Lastly figure out how to submit a pull request of your master to my master on github
+
+const { Person } = require("../../models/Person");
+const express = require("express");
+const router = express.Router();
+
+router.get("api/person", async (req, res) => {
+  try {
+    let people = await Person.find();
+    res.send(people);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.post("api/person", async (req, res) => {
+  try {
+    let person = new Person({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName
+    });
+    await person.save();
+    res.send(person);
+  } catch (error) {
+    console.error(error);
+  }
+});
