0361a80c3fe31cb7f33e48ff9f1ba6aaa0ed5052
John <johnmsaylor@gmail.com>
2020-03-03 11:29:19 -0500

    instructions for the person challenge

---
+++ routes/api/jms-person.js
@@ -0,0 +1,7 @@
+// copy this file and rename it <initial>-person.js
+// Add the get route to get the people from the People model
+// add the post route that will take a firstName and lastName from the body
+// this code should NOT be on master and should be done on a branch
+// push this branch to github ‘git push -u <remote> <branch>’
+// this branch needs to be merged to master. I can’t stop you from using git merge however this excersise is to practice pull requests. To complete this correctly merge the branch to master using the pull request system on your own repo
+// Lastly figure out how to submit a pull request of your master to my master on github
